241d719a0bdd8071097417737524af6b84a683bb
Robert Dunmire III <rdunmire@i-evolve.com>
2023-12-08 12:47:50 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/python3.10/dist-packages/nvidia/cudnn/lib/
-# python3 -m wyoming_piper \
-#     --piper '/usr/share/piper/piper' \
-#     --uri 'tcp://0.0.0.0:10200' \
-#     --length-scale "$(bashio::config 'length_scale')" \
-#     --noise-scale "$(bashio::config 'noise_scale')" \
-#     --noise-w "$(bashio::config 'noise_w')" \
-#     --speaker "$(bashio::config 'speaker')" \
-#     --voice "$(bashio::config 'voice')" \
-#     --max-piper-procs "$(bashio::config 'max_piper_procs')" \
-#     --data-dir /data \
-#     --data-dir /share/piper \
-#     --download-dir /data "$@"
 python3 -m wyoming_piper \
     --piper '/usr/share/piper/piper' \
     --uri 'tcp://0.0.0.0:10200' \
