ae40be2e410b5687cf435db74be546d94e1ef501
Robert Dunmire III <rdunmire@i-evolve.com>
2025-01-04 13:18:55 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/python3.10/dist-packages/nvidia/cudnn/lib/:/usr/local/lib/python3.10/dist-packages/nvidia/cublas/lib:/usr/local/lib/python3.10/dist-packages/nvidia/curand/lib/:/usr/local/lib/python3.10/dist-packages/nvidia/cufft/lib/:/usr/local/lib/python3.10/dist-packages/nvidia/cuda_runtime/lib/:/usr/local/lib/python3.10/dist-packages/nvidia/cuda_nvrtc/lib/
-python3 -m wyoming_piper \
+
+source /app/bin/activate
+/app/bin/python3 -m wyoming_piper \
     --piper '/usr/share/piper/piper' \
     --cuda \
     --uri 'tcp://0.0.0.0:10200' \
